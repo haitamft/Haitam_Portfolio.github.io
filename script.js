b4bc3b30ee18a9c97ea09daaf6a705badc478051
haitamft <118185349+haitamft@users.noreply.github.com>
2023-03-22 09:29:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed =new Typed('.taping', {
+    strings:["","WEB DEVELOPER","WEB DESIGNER",'FULL STACK DEVELOPER','FRONT-END DEVELOPER',"BACK-END DEVELOPER"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
